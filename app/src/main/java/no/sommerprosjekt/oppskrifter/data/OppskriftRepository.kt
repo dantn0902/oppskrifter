6c349eecac27d76fd680a8f1de7c6dbb3e6022ac
Dan Thanh Ngu <dantn0902@gmail.com>
2025-07-07 13:41:21 +0200

    Update and rename OppskriftRepository.kt to OppskriftRepositoryImp.kt

--- app/src/main/java/no/sommerprosjekt/oppskrifter/data/OppskriftRepository.kt
+++
@@ -1,7 +0,0 @@
-package no.sommerprosjekt.oppskrifter.data
-
-import no.sommerprosjekt.oppskrifter.domain.OppskriftRepositoryImp
-
-class OppskriftRepository() : OppskriftRepositoryImp {
-    init{}
-}
