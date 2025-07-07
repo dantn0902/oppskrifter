30ed60c7cd15979118f7ad72c873f60f5928cfff
Dan Thanh Ngu <dantn0902@gmail.com>
2025-07-07 14:07:41 +0200

    Update LagredeRepositoryImp.kt

--- app/src/main/java/no/sommerprosjekt/oppskrifter/data/LagredeRepositoryImp.kt
+++ app/src/main/java/no/sommerprosjekt/oppskrifter/data/LagredeRepositoryImp.kt
@@ -1,7 +1,13 @@
 package no.sommerprosjekt.oppskrifter.data
 
+import android.util.Log
+import kotlinx.coroutines.flow.StateFlow
 import no.sommerprosjekt.oppskrifter.domain.LagredeRepository
+import no.sommerprosjekt.oppskrifter.model.Oppskrift
 
 class LagredeRepositoryImp () : LagredeRepository {
     init{}
+    override fun getLagredeFlow() : StateFlow<List<Oppskrift>> {
+        TODO()
+    }
 }
