5fdf518fc86709e9a4b281cb76bfd66adda4872d
Dan Thanh Ngu <dantn0902@gmail.com>
2025-07-13 14:25:05 +0200

    Update LagredeRepositoryImp.kt
    skriver metode

--- app/src/main/java/no/sommerprosjekt/oppskrifter/data/LagredeRepositoryImp.kt
+++ app/src/main/java/no/sommerprosjekt/oppskrifter/data/LagredeRepositoryImp.kt
@@ -1,13 +1,15 @@
 package no.sommerprosjekt.oppskrifter.data
 
 import android.util.Log
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import no.sommerprosjekt.oppskrifter.domain.LagredeRepository
 import no.sommerprosjekt.oppskrifter.model.Oppskrift
 
 class LagredeRepositoryImp () : LagredeRepository {
     init{}
-    override fun getLagredeFlow() : StateFlow<List<Oppskrift>> {
-        TODO()
-    }
+    private val lagredeStateFlow = MutableStateFlow<List<Oppskrift>>(listOf())
+
+    override fun getLagrede() : StateFlow<List<Oppskrift>> = lagredeStateFlow.asStateFlow()
 }
