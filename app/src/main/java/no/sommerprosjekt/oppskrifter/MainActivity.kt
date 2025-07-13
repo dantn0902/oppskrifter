41b3609a63f74d26ff8c5cd56bd153e4e3954b8b
Dan Thanh Ngu <dantn0902@gmail.com>
2025-07-13 14:23:45 +0200

    Update MainActivity.kt

--- app/src/main/java/no/sommerprosjekt/oppskrifter/MainActivity.kt
+++ app/src/main/java/no/sommerprosjekt/oppskrifter/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import no.sommerprosjekt.oppskrifter.ui.LagredeScreen
+import no.sommerprosjekt.oppskrifter.ui.OppskriftScreen
 import no.sommerprosjekt.oppskrifter.ui.theme.OppskriterTheme
 
+@Suppress("DEPRECATION")
+//@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppContent()
                 }
             }
         }
@@ -30,17 +32,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OppskriterTheme {
-        Greeting("Android")
-    }
+fun AppContent() {
+    LagredeScreen()
 }
